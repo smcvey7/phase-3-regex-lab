f26c9bdd145e4ea3b391bed2cf3c2683b9792fce
Steven McVey <smcvey1210@gmail.com>
2022-12-10 01:49:10 -0800

    commit tree 32564df4334671db699fc18f70e9a492d5a5c0e8

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word.scan(/^[aeiouAEIOU]+\w+$/).length != 0
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w*.ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/^[A-Z]+.*.[.!?]$/).length != 0
 end
 
 def valid_phone_number?(phone)
-
+  p phone
+  p phone.scan(/\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}/).length != 0
+  phone.scan(/\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}/).length != 0
 end
